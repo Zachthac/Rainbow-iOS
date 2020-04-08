a6990b7e3a3735d8ef856c9fdf2c69313e5cc940
Your Name Here <youremailhere>
2020-04-07 22:34:25 -0400

    Set up color model

---
+++ Rainbow/Rainbow/Model/color.swift
@@ -0,0 +1,15 @@
+//
+//  color.swift
+//  Rainbow
+//
+//  Created by Clean Mac on 4/7/20.
+//  Copyright © 2020 LambdaStudent. All rights reserved.
+//
+
+import UIKit
+
+
+struct Color {
+    var colorName: String
+    var color: UIColor
+}
